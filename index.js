662884c2228f64efa4d784b54f4f335803f5ac4a
angelorepole <angelorepole@economist.com>
2015-07-08 13:54:47 +0100

    Linted

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ import ShareBar from '@economist/component-sharebar';
 
 export default class Gobbet extends React.Component {
 
+  static get propTypes() {
+    return {
+      title: React.PropTypes.string,
+      children: React.PropTypes.string,
+      showShareBar: React.PropTypes.string,
+      sharebar: React.PropTypes.string,
+    };
+  }
+
   render() {
     return (
       <div className="gobbet">
         {this.props.title ? <h1>{this.props.title}</h1> : null }
         {this.props.children}
-        {this.props.showShareBar === true ? <ShareBar {...this.props.sharebar}></ShareBar> : null }
+        {this.props.showShareBar === true ? <ShareBar {...this.props.sharebar} /> : null }
       </div>
     );
   }
